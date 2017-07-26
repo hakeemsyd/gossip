1a813745bb02330465c6567952ad8f09767cf543
Syed Hakeem Abbas <hakeemsyd@gmail.com>
2017-07-26 14:43:10 -0700

    Add fields to signup page & format code

--- app/layout/SignIn.js
+++ app/layout/SignIn.js
@@ -6,18 +6,18 @@ import { onSignIn } from "../auth";
 export default () => (
     //console.log('Hakeem is here');
 	<View style={{ paddingVertical: 20 }}>
-	<Card title="SIGN IN">
-	<FormLabel>Email</FormLabel>
-	<FormInput placeholder="Email address..." />
-	<FormLabel>Password</FormLabel>
-	<FormInput secureTextEntry placeholder="Password..." />
+		<Card title="SIGN IN">
+			<FormLabel>Email</FormLabel>
+			<FormInput placeholder="Email address..." />
+			<FormLabel>Password</FormLabel>
+			<FormInput secureTextEntry placeholder="Password..." />
 
-	<Button
-	buttonStyle={{ marginTop: 20 }}
-	backgroundColor="#03A9F4"
-	title="SIGN IN"
-	onPress={() => onSignIn()}
-	/>
-	</Card>
+			<Button
+				buttonStyle={{ marginTop: 20 }}
+				backgroundColor="#03A9F4"
+				title="SIGN IN"
+				onPress={() => onSignIn()}
+			/>
+		</Card>
 	</View>
 );
--- app/layout/SignUp.js
+++ app/layout/SignUp.js
@@ -5,16 +5,28 @@ import { onSignIn } from "../auth";
 
 export default ({ navigation }) => (
 	<View style={{ paddingVertical: 20 }}>
-	<Card>
-	{ /* ... */}
+		<Card>
+			<FormLabel>Email</FormLabel>
+			<FormInput placeholder="Email address..." />
+			<FormLabel>Password</FormLabel>
+			<FormInput secureTextEntry placeholder="Password..." />
 
-	<Button
-	buttonStyle={{ marginTop: 20 }}
-	backgroundColor="transparent"
-	textStyle={{ color: "#bcbec1" }}
-	title="Sign In"
-	onPress={() => navigation.navigate("SignIn")}
-	/>
-	</Card>
+			<FormLabel>Confirm Password</FormLabel>
+			<FormInput secureTextEntry placeholder="Confirm Password..." />
+
+			<Button
+				buttonStyle={{ marginTop: 20 }}
+				backgroundColor="#03A9F4"
+				title="SIGN UP"
+				onPress={() => onSignIn()}
+			/>
+			<Button
+				buttonStyle={{ marginTop: 20 }}
+				backgroundColor="transparent"
+				textStyle={{ color: "#bcbec1" }}
+				title="Sign In"
+				onPress={() => navigation.navigate("SignIn")}
+			/>
+		</Card>
 	</View>
 );
