8431750bb3e968b816f60f2e2f484f4c05c51b55
Syed Hakeem Abbas <hakeemsyd@gmail.com>
2017-07-26 18:38:11 -0700

    add artificats directory to git ignore and fix build

--- artifacts/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { SignedIn } from "./router";
-export default class App extends React.Component {
-    render() {
-        console.log('hakeem is here');
-        return React.createElement(SignedIn, null);
-    }
-}
-//# sourceMappingURL=index.js.map
