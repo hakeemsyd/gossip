19e22d3ad89c79ee1bb23f4782dfdf4b23fa0199
Syed Hakeem Abbas <hakeemsyd@gmail.com>
2017-07-26 17:20:21 -0700

    switch to class components

--- app/layout/Home.js
+++ app/layout/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { ScrollView, Text, Linking, View } from "react-native";
 import { Card, Button } from "react-native-elements";
 
@@ -29,21 +29,27 @@ const images = [
     }
 ];
 
-export default () => (
-    <View style={{ flex: 1 }}>
-        <ScrollView contentContainerStyle={{ paddingVertical: 20 }}>
-            {images.map(({ name, image, url, key }) => (
-                <Card title={`CARD ${key}`} image={image} key={key}>
-                    <Text style={{ marginBottom: 10 }}>
-                        Photo by {name}.
+class Home extends Component {
+    render() {
+        return (
+            <View style={{ flex: 1 }}>
+                <ScrollView contentContainerStyle={{ paddingVertical: 20 }}>
+                    {images.map(({ name, image, url, key }) => (
+                        <Card title={`CARD ${key}`} image={image} key={key}>
+                            <Text style={{ marginBottom: 10 }}>
+                                Photo by {name}.
           </Text>
-                    <Button
-                        backgroundColor="#03A9F4"
-                        title="VIEW NOW"
-                        onPress={() => Linking.openURL(url)}
-                    />
-                </Card>
-            ))}
-        </ScrollView>
-    </View>
-);
+                            <Button
+                                backgroundColor="#03A9F4"
+                                title="VIEW NOW"
+                                onPress={() => Linking.openURL(url)}
+                            />
+                        </Card>
+                    ))}
+                </ScrollView>
+            </View>
+        )
+    }
+};
+
+module.exports = Home;
