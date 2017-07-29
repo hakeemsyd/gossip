1ae1510035b72d602b30e84e4f6af9e53025dd87
Syed Hakeem Abbas <hakeemsyd@gmail.com>
2017-07-29 01:11:09 -0700

    Move images as part of build

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,14 @@ var ts = require('gulp-typescript');
 // Grab settings from tsconfig.json
 var tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('build', function () {
+gulp.task('build', ['assets'], function () {
     var tsResult = tsProject.src().pipe(tsProject());
     return tsResult.js.pipe(gulp.dest('bin'));
+});
+
+gulp.task('assets', function () {
+    gulp.src("./app/images/**.*")
+        .pipe(gulp.dest('bin/app/images'));
 });
 
 gulp.task('watch', ['build'], function () {
